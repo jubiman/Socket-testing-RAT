0ba6d6ee6d0d58fc8ae4d603896b083c4859c3ee
Melle Bosboom <47221652+jubiman@users.noreply.github.com>
2020-01-01 15:03:50 +0100

    Update server.cpp

--- Server/server.cpp
+++ Server/server.cpp
@@ -8,7 +8,8 @@
 #include <sstream>     // Converting variables
 #include <fstream>     // File in-/output 
 #include <thread>      // Multi-threading
-#include <atomic>      // For atomic booleans that can be read thread-to-thread 
+#include <atomic>      // For atomic booleans that can be read thread-to-thread
+#include <mutex>       // Locking and unlocking console
 //#include <future>      // Thread return values
 
 // Boost includes
@@ -39,35 +40,55 @@ struct path_leaf_string
 std::string serverip = "192.168.0.8"; // IP adress server will be running on, if not local
 int port = 5555;                      // Port the server will be running on
 char buffer[1024];                    // Buffer with max lenght of 1024 bytes
-char pongBuffer[1024] = "pong";      // Pong buffer containing message "pong"
+char pongBuffer[1024] = "pong";       // Pong buffer containing message "pong"
 char pingBuffer[1024];                // Ping buffer (will be used on recv end from client) 
 bool debug = false;                   // Show debug information on/off (default == off)
 
 // Initializing socket variables
 WSADATA WSAData;
-SOCKET server, client;
+SOCKET server, gclient;
 SOCKADDR_IN serverAddr, clientAddr;
+std::vector<SOCKET> clients;
+
+#ifdef __linux__
+unsigned const long getIP() { return address.sin_addr.s_addr; }
+#else
+unsigned const long getIP() { return (clientAddr.sin_addr.S_un.S_un_b.s_b1 << 24) | (clientAddr.sin_addr.S_un.S_un_b.s_b2 << 16) | (clientAddr.sin_addr.S_un.S_un_b.s_b3 << 8) | clientAddr.sin_addr.S_un.S_un_b.s_b4; }
+#endif 
 
 // Atomic boolean
 std::atomic<bool> terminate_thread(false);
 std::atomic<bool> disconnect(false);
 std::atomic<bool> uptimeCalled(false);
 
-// Defining thread functions
-void serverHandler(int argc, const char* argv[]);
-void inputHandler();
-void uptimeHandler();
+// Mutex
+std::mutex mu;
 
-									/********************************\
-									|								 |
-									|   TODO: Make receive handler   |
-									|								 |
-									|                                |
-									\********************************/
+// Defining thread functions
+void serverHandler(int, char*[], SOCKET);
+void inputHandler(SOCKET);
+void uptimeHandler();
+void serverInit();
+void connectionHandler();
+void ClientSession(SOCKET);
+unsigned __stdcall ClientSession(void*);
+
+/********************************\
+|								 |
+|   TODO: Make receive handler   |
+|								 |
+|                                |
+\********************************/
+
+// Global argc and argv values
+int gargc;
+char** gargv;
 
 
 // Main function
-int main(int argc, const char* argv[]) {
+int main(int argc, char* argv[]) {
+	::gargc = argc;
+	::gargv = argv;
 	// Algorithm for checking arguments
 	if (argc > 0) {
 		for (int i = 1; i < argc; ++i) {
@@ -92,22 +113,17 @@ int main(int argc, const char* argv[]) {
 		}
 	}
 
+	// Initialize server
+	serverInit();
 
+	// Handle all connections untill terminate_conn is called
+	connectionHandler();
 
-	std::thread servHndlr(serverHandler, argc, argv);  // Creating thread for the server handler function
-	std::thread inputHndlr(inputHandler);              // Creating thread for the input handler function
-	std::thread uptimeHndlr(uptimeHandler);            // Creating thread for the uptime handler function
+	std::thread inputHndlr(inputHandler, gclient); // Creating thread for the input handler function
+	std::thread uptimeHndlr(uptimeHandler);              // Creating thread for the uptime handler function
 
-	servHndlr.join();   // Joining server handler thread to run on new thread
 	inputHndlr.join();  // Joining input handler thread to run on new thread
 	uptimeHndlr.join(); // Joining uptime handler thread to run on new thread
-
-	while (!terminate_thread); // Check if threads need to be terminated
-	std::this_thread::sleep_for(std::chrono::seconds(1));
-	servHndlr.detach();        // Terminate server handler thread
-	inputHndlr.detach();       // Terminate input handler thread
-	uptimeHndlr.detach();      // Terminate uptime handler thread
-
 
 	// Locking the console until keypress
 	char input;
@@ -115,9 +131,27 @@ int main(int argc, const char* argv[]) {
 	return 0;
 }
 
+void terminate_conn(SOCKET _client) {
+	closesocket(_client);
+	closesocket(server);
+	WSACleanup();
+	disconnect = true;
+	terminate_thread = true;
+	return;
+}
 
-// Initializing thread functions
-void serverHandler(int argc, const char* argv[]) {
+void terminate_conn() {
+	for (size_t i = 0; i < clients.size(); ++i) {
+		closesocket(clients[i]);
+	}
+	closesocket(server);
+	WSACleanup();
+	disconnect = true;
+	terminate_thread = true;
+	return;
+}
+
+void serverInit() {
 	int iResult = WSAStartup(MAKEWORD(2, 0), &WSAData);
 	server = socket(AF_INET, SOCK_STREAM, 0);
 	if (iResult != 0) {
@@ -131,22 +165,96 @@ void serverHandler(int argc, const char* argv[]) {
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(port);
 
-	bind(server, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
+	iResult = bind(server, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
+	if (iResult == SOCKET_ERROR) {
+		std::cout << "Bind failed with error: \n" << WSAGetLastError() << std::endl;
+		closesocket(server);
+	}
 	listen(server, 0);
-
 	std::cout << "Listening for incoming connections on \"" << serverip << ":" << port << "\"..." << std::endl;
+	return;
+}
 
-	int clientAddrSize = sizeof(clientAddr);
-	if ((client = accept(server, (SOCKADDR*)& clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
+unsigned __stdcall ClientSession(void* data)
+{
+	SOCKET client_socket = (SOCKET)data;
+
+	std::thread servHndlr(serverHandler, gargc, gargv, client_socket);  // Creating thread for the server handler function
+	std::thread inputHndlr(inputHandler, client_socket); // Creating thread for the input handler function
+	std::thread uptimeHndlr(uptimeHandler);              // Creating thread for the uptime handler function
+
+	servHndlr.join();   // Joining server handler thread to run on new thread
+	inputHndlr.join();  // Joining input handler thread to run on new thread
+	uptimeHndlr.join(); // Joining uptime handler thread to run on new thread
+
+	while (!terminate_thread); // Check if threads need to be terminated
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	servHndlr.detach();        // Terminate server handler thread
+	inputHndlr.detach();       // Terminate input handler thread
+	uptimeHndlr.detach();      // Terminate uptime handler thread
+	return 0;
+}
+
+void ClientSession(SOCKET data) {
+	std::thread servHndlr(serverHandler, gargc, gargv, data);  // Creating thread for the server handler function
+	std::thread inputHndlr(inputHandler, data); // Creating thread for the input handler function
+	std::thread uptimeHndlr(uptimeHandler);              // Creating thread for the uptime handler function
+
+	servHndlr.join();   // Joining server handler thread to run on new thread
+	inputHndlr.join();  // Joining input handler thread to run on new thread
+	uptimeHndlr.join(); // Joining uptime handler thread to run on new thread
+
+	while (!terminate_thread); // Check if threads need to be terminated
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	servHndlr.detach();        // Terminate server handler thread
+	inputHndlr.detach();       // Terminate input handler thread
+	uptimeHndlr.detach();      // Terminate uptime handler thread
+}
+
+int clientAddrSize = 0;
+
+// Initializing thread functions
+void connectionHandler() {
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " connectionHandler running" << std::endl;
+	mu.unlock();
+	while (!terminate_thread) {
+		clientAddrSize = sizeof(clientAddr);
+		if ((gclient = accept(server, (SOCKADDR*)&clientAddr, &clientAddrSize)) == INVALID_SOCKET) {
+			std::cout << "Could not accept client" << std::endl;
+		}
+		else {
+			std::cout << "Accepted new client at " << getIP() << ":" << serverAddr.sin_port << "." << std::endl;
+			std::cout << "\nClient info:\n" << "\tIPv4: " << clientAddr.sin_addr.S_un.S_addr << "\n\tPort: " << clientAddr.sin_port << "\n\tClient: " << gclient << std::endl << std::endl;;
+			
+			clients.push_back(gclient);
+
+			unsigned threadID;
+			HANDLE hThread = (HANDLE)_beginthreadex(NULL, 0, &ClientSession, (void*)gclient, 0, &threadID);
+		}
+	}
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " connectionHandler stopped running" << std::endl;
+	mu.unlock();
+}
+
+void serverHandler(int argc, char* argv[], SOCKET client) {
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " serverHandler running" << std::endl;
+	mu.unlock();
+	clientAddrSize = sizeof(clientAddr);
+	if ((client = accept(server, (SOCKADDR*)&clientAddr, &clientAddrSize)) != INVALID_SOCKET) {
 		std::cout << "Client connected!" << std::endl;
 		while (!terminate_thread && !disconnect) {
 			recv(client, buffer, sizeof(buffer), 0);
+			std::cout << "buffer = " << buffer << std::endl;
 			if (buffer != NULL && buffer[0] != '\0') {
 				if ((strcmp(buffer, "disconnect") == 0) || (strcmp(buffer, "dc") == 0) || disconnect) {
-					closesocket(client); // Disconnecting socket
+					closesocket(client); // Disconnecting client
+					closesocket(server); // Disconnecting server
 					std::cout << "Client disconnected." << std::endl;
 					terminate_thread = true;
-					break; // Stops the infinite loop to prevent sending messages to unconnected server
+					break; // Stops the infinite loop to prevent receiving messages from unconnected client
 				}
 				else {
 					// Algorithm that puts string into a vector<string> array to have command and argument(s) seperated
@@ -157,10 +265,13 @@ void serverHandler(int argc, const char* argv[]) {
 					if (splitCommand[0] == "ping") {
 						char rcvbuf[1024];
 						recv(client, rcvbuf, sizeof(rcvbuf), 0);
+						std::cout << "rcvbuff = " << rcvbuf << std::endl;
 						send(client, pongBuffer, sizeof(pongBuffer), 0);
 						recv(client, pingBuffer, sizeof(pingBuffer), 0);
+						std::cout << "pingBuffer = " << pingBuffer << std::endl;
 						std::cout << "Ping: " << pingBuffer << " ms" << std::endl;
 						memset(rcvbuf, 0, sizeof(rcvbuf));
+						std::cout << "Ping completed" << std::endl;
 					}
 					else if (splitCommand[0] == "pwd") {
 						char buf[1024];
@@ -271,25 +382,61 @@ void serverHandler(int argc, const char* argv[]) {
 			}
 		}
 	}
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " serverHandler stopped running" << std::endl;
+	mu.unlock();
 }
 
-void inputHandler() {
+void inputHandler(SOCKET client_socket) {
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " inputHandler running" << std::endl;
+	mu.unlock();
 	while (!terminate_thread) {
 		std::string input;
 		getline(std::cin, input);
-		if (input == "disconnect" || input == "dc") {
+		boost::replace_all(input, " ", "\t");
+		std::vector<std::string> splitCommand;
+		boost::split(splitCommand, input, boost::is_any_of("\t")); // splitCommand[0] is main command, from splitCommand[1] are argvs
+		if (splitCommand[0] == "disconnect" || splitCommand[0] == "dc") {
+			if (splitCommand.size < 2) {
+				std::cout << "You need at least 1 argument" << std::endl;
+				continue;
+			}
 			char dcBuffer[1024] = "disconnect";
-			send(client, dcBuffer, sizeof(dcBuffer), 0);
-			std::cout << "disconnected from client" << std::endl;
-			disconnect = true;
+			if (splitCommand[1] == "all") {
+				for (size_t i = 0; i < clients.size(); ++i) {
+					send(clients[i], dcBuffer, sizeof(dcBuffer), 0);
+				}
+				std::cout << "Disconnected from client" << std::endl;
+				disconnect = true;
+			}
+			else {
+				int i = 0;
+				std::stringstream iss;
+				iss << splitCommand[1];
+				iss >> i;
+				send(clients[i], dcBuffer, sizeof(dcBuffer), 0);
+				std::cout << "Disconnected from client" << std::endl;
+				disconnect = true;
+			}
 			memset(dcBuffer, 0, sizeof(dcBuffer));
+			std::cout << "Thread id: " << std::this_thread::get_id() << " inputHandler stopped running" << std::endl;
 			return;
 		}
+		else if (splitCommand[0] == "uptime") {
+			uptimeCalled = true;
+		}
 	}
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " inputHandler stopped running" << std::endl;
+	mu.unlock();
 	return;
 }
 
 void uptimeHandler() {
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " uptimeHandler running" << std::endl;
+	mu.unlock();
 	auto start = std::chrono::high_resolution_clock::now();
 	while (!terminate_thread && !disconnect) {
 		if (!uptimeCalled) continue;
@@ -299,4 +446,7 @@ void uptimeHandler() {
 			uptimeCalled = false;
 		}
 	}
+	mu.lock();
+	std::cout << "Thread id: " << std::this_thread::get_id() << " uptimeHandler stopped running" << std::endl;
+	mu.unlock();
 }
